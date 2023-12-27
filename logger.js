976df28e393779f2ec83f042101723258b26a620
_baget <96415977+baget-underscore@users.noreply.github.com>
2023-12-27 20:01:37 +0000

    Made logging prettier

--- logger.js
+++ logger.js
@@ -4,6 +4,7 @@ const logger = pino({
         target: 'pino-pretty',
         options: {
             colorize: true,
+            customColors: 'err:red,info:blue',
             ignore: 'pid,hostname',
             include: 'level,time,msg',
             translateTime: 'HH:mm:ss',
