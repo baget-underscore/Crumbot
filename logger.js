8ece8f097b20fa6a10b77ca3e30c9531b0719d1e
_baget <96415977+baget-underscore@users.noreply.github.com>
2023-12-27 18:00:45 +0000

    Fixed logger, looks good now

--- logger.js
+++ logger.js
@@ -1,3 +1,16 @@
 const pino = require('pino');
-const logger = pino();
+const oldlogger = pino();
+const logger = pino({
+    transport: {
+        target: 'pino-pretty',
+        options: {
+            colorize: true,
+            ignore: 'pid,hostname',
+            include: 'level,time,msg',
+            translateTime: 'HH:mm:ss',
+        }
+    },
+});
+oldlogger.info('Test');
+logger.info('Test');
 module.exports = logger;
