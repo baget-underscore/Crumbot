e388281780964ff41a616b5cb431cd7eb4747f51
_baget <96415977+baget-underscore@users.noreply.github.com>
2023-12-27 20:27:33 +0000

    Fixed emoji

--- commands/creators/create.js
+++ commands/creators/create.js
@@ -53,7 +53,7 @@ function createButtons(creator, buttons = []) {
 
 async function getImage(interaction) {
     return new Promise(resolve => {
-        interaction.channel.send({ content: `<a:loading_y:1138773326945341481> ${interaction.user} **reply** to this message with an image or image URL`, fetchReply: true })
+        interaction.channel.send({ content: `<a:waiting:${emoji[waiting]}> ${interaction.user} **reply** to this message with an image or image URL`, fetchReply: true })
         .then((reply) => {
             const collector = interaction.channel.createMessageCollector({
                 filter: res => res.author.id === interaction.user.id && res.reference ? res.reference.messageId === reply.id : false,
@@ -70,7 +70,7 @@ async function getImage(interaction) {
                         resolve([m, file]);
                     }
                     else {
-                        await reply.edit({ content: `<a:no:1108997977651032094> \`${attachment.name}\` is not an image, try again.` });
+                        await reply.edit({ content: `<a:error:${emoji[error]}> \`${attachment.name}\` is not an image, try again.` });
                     }
                 }
                 else if (m.content.match(/https:\/\/(.*)(png|jpg|jpeg)/)) {
@@ -80,7 +80,7 @@ async function getImage(interaction) {
                     resolve([m, file]);
                 }
                 else {
-                    await reply.edit({ content: `<a:no:1108997977651032094> \`${m.content}\` is not an image, try again.` });
+                    await reply.edit({ content: `<a:error:${emoji[error]}> \`${m.content}\` is not an image, try again.` });
                 }
             });
         });
